66531b38672059ee6961c8e2691d282fe41c2411
Hugo Haldi <20846785+ShinoYasx@users.noreply.github.com>
2020-11-12 18:47:16 +0100

    Fixed a bug in map_find and improved map_insert

--- Containers/map.c
+++ Containers/map.c
@@ -32,7 +32,8 @@ pair_bool map_insert(map_t *map, const value_t value) {
     map_iterator_t *it = map->root;
 
     while (it) {
-        if (map->key_compare(value.key, it->value.key) < 0) {
+        int comp = map->key_compare(value.key, it->value.key);
+        if (comp < 0) {
             if (!it->left) {
                 it->left = map_iterator_factory(value);
                 it->left->parent = it;
@@ -41,18 +42,15 @@ pair_bool map_insert(map_t *map, const value_t value) {
                 break;
             }
             it = it->left;
-            continue;
-        }
-        if (map->key_compare(value.key, it->value.key) > 0) {
+        } else if (comp > 0) {
             if (!it->right) {
                 it->right = map_iterator_factory(value);
                 it->right->parent = it;
                 break;
             }
             it = it->right;
-            continue;
-        }
-        return make_pair(it, false); // Appends when the key is equal to an existing one
+        } else
+            return make_pair(it, false); // Appends when the key is equal to an existing one
     }
     return make_pair(it, true);
 }
@@ -70,7 +68,7 @@ map_iterator_t *map_find(map_t *map, const void *key) {
             return it;
         if (map->key_compare(key, it->value.key) < 0)
             it = it->left;
-        if (map->key_compare(key, it->value.key) > 0)
+        else if (map->key_compare(key, it->value.key) > 0)
             it = it->right;
     }
     return it; // Return NULL if not found
